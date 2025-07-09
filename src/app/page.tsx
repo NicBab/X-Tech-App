324075807056a120d56520e0fca5f4ed7ac7b392
nickpbab@gmail.com <Nick_Bab>
2025-07-09 16:23:23 -0500

    failed shadcnui theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,12 @@ import DLRForm from "./(components)/Forms/DLRForm";
 import Login from "./(components)/Forms/Login"
 import Register from "./(components)/Forms/Register"
 
+
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-10 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[20px] row-start-2 items-center">
+      <main className="flex flex-col row-start-2 items-center">
+
         <Login/>
         <Register/>
         <DLRForm/>
