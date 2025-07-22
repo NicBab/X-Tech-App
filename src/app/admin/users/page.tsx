0dc9b2aba21c8e83106966a85e6e82fa312d5f3c
Nic Babineaux <76629649+NicBab@users.noreply.github.com>
2025-07-21 19:56:48 -0500

    still no render for inventory or products

--- src/app/admin/users/page.tsx
+++ src/app/admin/users/page.tsx
@@ -1,13 +1,11 @@
 "use client";
 
 import UsersTable from "@/app/(components)/Tables/UsersTable";
-import Header from "@/app/(components)/Header";
 
 export default function UsersPage() {
   return (
     <div className="p-6">
       <div className="flex justify-between items-center mb-6">
-        <Header name="Users" />
       </div>
       <UsersTable />
     </div>
